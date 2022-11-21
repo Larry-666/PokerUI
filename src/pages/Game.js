d76fe65f966abb120466ab77cc8880cb3c1a7f52
Lauri Latonummi <78909240+Larry-666@users.noreply.github.com>
2022-11-21 12:17:35 +0200

    'project'

---
+++ src/pages/Game.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Game.css';
+
+function Game() {
+
+ return(
+  <>
+  <nav className='navbar'>
+    <h1>Poker Game</h1>
+
+     
+     <button> Options</button>
+  </nav>
+ </>
+  
+   )
+ }
+
+ export default Game
+
